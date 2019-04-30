af8d3951849054637e8c2f6338f0525e8b9c1223
grzegorzb <grzegorzb@medius-group.com>
2019-04-30 16:17:13 +0200

    Added Password in Memory example

---
+++ src/PasswordInMemoryExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordInMemoryExample
+{
+    class Program
+    {
+        private static Random  _rand = new Random();
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 100000; i++)
+            {
+                AcceptPasswordAsync();
+            }
+            Console.WriteLine("Press any key to kill the process.");
+            Console.ReadKey();
+        }
+
+        private static async Task AcceptPasswordAsync()
+        {
+            await Task.Run(
+                () =>
+                {
+                    var input = "MySecretPassword" + _rand.Next(0, 1000);
+                    Console.WriteLine(input.EndsWith("4") ? "Access granted!" : "Access denied.");
+                });
+        }
+    }
+}
---
+++ src/PasswordInMemoryExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PasswordInMemoryExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PasswordInMemoryExample")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f0fddef9-6ced-4ab9-98ed-f4a16937b109")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
